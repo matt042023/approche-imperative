3b160637058809f7eb59fffa20cbc0180bf2a998
matt042023 <matt.lheureux@hotmail.fr>
2025-07-07 15:30:11 +0200

    Tp5 - tableaux print amelioré

--- src/fr/tableaux/ExerciceDeclarationTab.java
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -4,8 +4,9 @@ public class ExerciceDeclarationTab {
     public static void main(String[] args) {
         int[] entier = { 8, -7, 12, 1, -2, 14, 17, 9 };
 
-        System.out.println(entier[0]);
         System.out.println(entier.length);
+        System.out.println(entier[0]);
+        System.out.println(entier[entier.length -1]);
 
         // Le code echou car il n'y a pas de valeur à l'index 10 du tableau
         System.out.println(entier[10]);
--- src/fr/tableaux/ExerciceTableauChaines.java
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -9,13 +9,27 @@ public class ExerciceTableauChaines {
         System.out.println(villes[2]);
         System.out.println(villes[3]);
         System.out.println(villes[4]);
-        System.out.println(villes.length);
-        villes[3] = "Reims";
-        System.out.println(villes[0]);
-        System.out.println(villes[1]);
-        System.out.println(villes[2]);
-        System.out.println(villes[3]);
-        System.out.println(villes[4]);
+        System.out.println(" ");
+        System.out.println("------------------------------------------");
+        System.out.println(" ");
+        System.out.println("longeur du tableau : " + villes.length);
 
+        System.out.println(" ");
+        System.out.println("------------------------------------------");
+        System.out.println(" ");
+
+        villes[3] = "Reims";
+
+        for (String ville : villes) {
+            System.out.println(ville);
+        }
+
+
+        // equivalent à :
+        /*
+         * for (int i = 0; i < villes.length; i++) {
+         *  System.out.println(villes[i]);
+         * }
+         * */
     }
 }
