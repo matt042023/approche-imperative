446a2d487c189de6511fc4a23f02fe7ac5513d86
matt042023 <matt.lheureux@hotmail.fr>
2025-07-07 11:53:40 +0200

    declaration de valiable

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,38 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte byteTest = 2;
+
+        short shortTest = 45;
+
+        int intTest = 514;
+
+        long longTest = 5412458;
+
+        float floatTest = 2.5F;
+
+        double doubleTest = 23.21;
+
+        char charTest = 'G';
+
+        boolean booleanTest = true;
+
+        String stringTest = "Hello Word";
+
+        System.out.println("byte : " + byteTest);
+        System.out.println("short : " + shortTest);
+        System.out.println("int : " + intTest);
+        System.out.println("float : " + floatTest);
+        System.out.println("double : " + doubleTest);
+        System.out.println("char : " + charTest);
+        System.out.println("boolean : " + booleanTest);
+        System.out.println("string : " + stringTest);
+
+
+        String randomString = "Voici le résultat d'un calcule : \n 1 + 5 = 6";
+        System.out.println(randomString);
+
+
+    }
+}
