f6b6503b66f0036fb2fbb029525a1658ff11bb47
matt042023 <matt.lheureux@hotmail.fr>
2025-07-08 17:16:35 +0200

    tp-7 inversion contenu

---
+++ src/fr/algorithmie/InversionContenu.java
@@ -0,0 +1,18 @@
+package fr.algorithmie;
+
+import java.util.Arrays;
+
+public class InversionContenu {
+    public static void main(String[] args) {
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        int[] arrayCopy = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            arrayCopy[i] = array[array.length - 1 - i];
+        }
+
+        System.out.println("Tableau original : " + Arrays.toString(array));
+        System.out.println("----------------");
+        System.out.println("Tableau inverse : " + Arrays.toString(arrayCopy));
+    }
+}
